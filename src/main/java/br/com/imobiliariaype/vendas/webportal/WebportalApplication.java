fbd4506d44058aa3cf52eeaf06afaf1cd6b3c127
Johnnes Souza <jhonnesf5@gmail.com>
2019-09-28 10:39:24 -0300

    Project archetype

---
+++ src/main/java/br/com/imobiliariaype/vendas/webportal/WebportalApplication.java
@@ -0,0 +1,13 @@
+package br.com.imobiliariaype.vendas.webportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebportalApplication.class, args);
+	}
+
+}
