98f86b31f305bd0f6c22d97311a9c84db1a58583
Johnnes Souza <johnnes@ciandt.com>
2019-09-30 13:41:46 -0300

    Swagger Documentation

--- src/main/java/br/com/imobiliariaype/vendas/webportal/WebportalApplication.java
+++ src/main/java/br/com/imobiliariaype/vendas/webportal/WebportalApplication.java
@@ -2,8 +2,12 @@ package br.com.imobiliariaype.vendas.webportal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
+@EnableSwagger2WebFlux
+@EnableAsync
 public class WebportalApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/imobiliariaype/vendas/webportal/controller/CustomerController.java
+++ src/main/java/br/com/imobiliariaype/vendas/webportal/controller/CustomerController.java
@@ -2,6 +2,7 @@ package br.com.imobiliariaype.vendas.webportal.controller;
 
 import br.com.imobiliariaype.vendas.webportal.model.Customer;
 import br.com.imobiliariaype.vendas.webportal.service.CustomerService;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,18 +25,21 @@ public class CustomerController {
         this.customerService = customerService;
     }
 
+    @ApiOperation(value = "Save a new customer", response = Customer.class)
     @PostMapping("/customers")
     public Mono<Customer> saveClient(@Valid @RequestBody Customer customer){
         LOGGER.info("Saving new Customer with id [{}]", customer.getId());
         return customerService.saveCustomer(customer);
     }
 
+    @ApiOperation(value = "Find a customer")
     @GetMapping("/customers/{customer_id}")
     public Mono<Customer> findById(@PathVariable String customer_id){
         LOGGER.info("Looking for Customer with id [{}]", customer_id);
         return customerService.findCustomerById(customer_id);
     }
 
+    @ApiOperation(value = "Delete a customer")
     @DeleteMapping("/customers/{customer_id}")
     public Mono<Customer> deleteCustomer(@PathVariable String customer_id){
         LOGGER.info("Delete customer with id [{}]", customer_id);
